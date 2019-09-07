196ed20c21ca56098eba7383dcc526aa60667c06
shinyay <shinya.com@gmail.com>
2019-09-07 19:15:48 +0900

    Add http access test with TestRestTemplate

--- src/test/kotlin/io/pivotal/shinyay/SpringCloudAppHelloWorldApplicationTests.kt
+++ src/test/kotlin/io/pivotal/shinyay/SpringCloudAppHelloWorldApplicationTests.kt
@@ -1,13 +1,18 @@
 package io.pivotal.shinyay
 
+import org.assertj.core.api.Assertions.*
 import org.junit.jupiter.api.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-class SpringCloudAppHelloWorldApplicationTests {
+class SpringCloudAppHelloWorldApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
 
 	val logger: Logger = LoggerFactory.getLogger(this.javaClass.name)
 
@@ -22,7 +27,16 @@ class SpringCloudAppHelloWorldApplicationTests {
 	}
 
 	@Test
-	fun contextLoads() {
+	fun given_when_then() {
+		logger.info("GIVEN: Preparation -> The state is X")
+		logger.info("WHEN: Execution -> Something happens")
+		logger.info("THEN: Verification -> The result is Y")
+	}
+
+	@Test
+	fun httpClient_accessContextRoot_statusCodeOK() {
+		val entity = restTemplate.getForEntity<String>("/")
+		assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
 	}
 
 	@AfterEach
