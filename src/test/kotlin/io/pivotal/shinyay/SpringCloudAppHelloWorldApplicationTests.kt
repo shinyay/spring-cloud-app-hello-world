96aef21574010f2f0950f4c55ececbae32e62feb
shinyay <shinya.com@gmail.com>
2019-09-07 18:02:33 +0900

    Add prepare functions for test

--- src/test/kotlin/io/pivotal/shinyay/SpringCloudAppHelloWorldApplicationTests.kt
+++ src/test/kotlin/io/pivotal/shinyay/SpringCloudAppHelloWorldApplicationTests.kt
@@ -1,13 +1,37 @@
 package io.pivotal.shinyay
 
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.*
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class SpringCloudAppHelloWorldApplicationTests {
+
+	val logger: Logger = LoggerFactory.getLogger(this.javaClass.name)
+
+	@BeforeAll
+	fun initialize() {
+		logger.info(">> Setup for Integration Test")
+	}
+
+	@BeforeEach
+	fun beforeEach() {
+		logger.info(">> Setup for Each Test")
+	}
 
 	@Test
 	fun contextLoads() {
 	}
 
+	@AfterEach
+	fun afterEach() {
+		logger.info(">> Teardown for Each Test")
+	}
+
+	@AfterAll
+	fun afterAll() {
+		logger.info(">> Teardown for Integration Test")
+	}
 }
