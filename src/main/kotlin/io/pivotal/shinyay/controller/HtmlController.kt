597ecbf22db8a9a6099d9a469878c00ce774706a
shinyay <shinya.com@gmail.com>
2019-09-07 15:39:46 +0900

    Add Controller for Hello view

---
+++ src/main/kotlin/io/pivotal/shinyay/controller/HtmlController.kt
@@ -0,0 +1,16 @@
+package io.pivotal.shinyay.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+    @GetMapping("/")
+    fun html(model: Model): String {
+        model["title"] = "Hello"
+        return "Hello"
+    }
+}
