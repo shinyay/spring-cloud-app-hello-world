50cd836436ba884729f7ca09e3be706d3f0bd46f
shinyay <shinya.com@gmail.com>
2019-09-08 22:31:01 +0900

    Add test for article repository

--- src/test/kotlin/io/pivotal/shinyay/RepositoryTest.kt
+++ src/test/kotlin/io/pivotal/shinyay/RepositoryTest.kt
@@ -1,5 +1,6 @@
 package io.pivotal.shinyay
 
+import io.pivotal.shinyay.entity.Article
 import io.pivotal.shinyay.entity.User
 import io.pivotal.shinyay.repository.ArticleRepository
 import io.pivotal.shinyay.repository.UserRepository
@@ -8,11 +9,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.data.repository.findByIdOrNull
 
 @DataJpaTest
 class RepositoryTest @Autowired constructor(
         val entityManager: TestEntityManager,
-        val userRepository: UserRepository) {
+        val userRepository: UserRepository,
+        val articleRepository: ArticleRepository) {
 
     @Test
     fun givenUser_whenFindByLogin_thenReturnUser() {
@@ -20,5 +23,20 @@ class RepositoryTest @Autowired constructor(
         entityManager.persistAndFlush(shinyay)
         val actualUser = userRepository.findByLogin(shinyay.login)
         assertThat(actualUser).isEqualTo(shinyay)
+    }
+
+    @Test
+    fun givenUserAndArticle_whenFindByIdOrNull_thenReturnArticle() {
+        val shinyay = User("shinyay", "Shinya", "Yanaihara")
+        entityManager.persist(shinyay)
+        val article = Article(
+                "Hello Spring",
+                "Spring with Kotlin",
+                "https://github.com/shinyay/spring-cloud-app-hello-world",
+                shinyay)
+        entityManager.persist(article)
+        entityManager.flush()
+        val actualArticle = articleRepository.findByIdOrNull(article.id!!)
+        assertThat(actualArticle).isEqualTo(article)
     }
 }
