3ea3467b69ea0b6f080e46f785da08e29be1c21c
shinyay <shinya.com@gmail.com>
2019-09-08 20:43:34 +0900

    Add entity for Article with type extended kotlin function

---
+++ src/main/kotlin/io/pivotal/shinyay/entity/Article.kt
@@ -0,0 +1,19 @@
+package io.pivotal.shinyay.entity
+
+import io.pivotal.shinyay.extensions.toSlug
+import java.time.LocalDateTime
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+import javax.persistence.ManyToOne
+
+@Entity
+data class Article(
+        var title: String,
+        var headline: String,
+        var content: String,
+        @ManyToOne var author: User,
+        var slug: String = title.toSlug(),
+        var addedAt: LocalDateTime = LocalDateTime.now(),
+        @Id @GeneratedValue var id: Long? = null
+)
