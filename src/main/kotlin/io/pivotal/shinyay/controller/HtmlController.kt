1219c5d74211f7e462e2f02e122a959e08ff0f92
shinyay <shinya.com@gmail.com>
2019-09-09 07:39:29 +0900

    Add retrieve aricle info from repository

--- src/main/kotlin/io/pivotal/shinyay/controller/HtmlController.kt
+++ src/main/kotlin/io/pivotal/shinyay/controller/HtmlController.kt
@@ -1,16 +1,38 @@
 package io.pivotal.shinyay.controller
 
+import io.pivotal.shinyay.entity.Article
+import io.pivotal.shinyay.entity.User
+import io.pivotal.shinyay.extensions.format
+import io.pivotal.shinyay.repository.ArticleRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HtmlController {
+class HtmlController(val repository: ArticleRepository) {
 
     @GetMapping("/")
     fun html(model: Model): String {
         model["title"] = "Hello"
-        return "Hello"
+        model["articles"] = repository.findAllByOrderByAddedAtDesc().map { it.render() }
+        return "hello"
     }
+
+    data class RenderedArticle(
+            val slug: String,
+            val title: String,
+            val headline: String,
+            val content: String,
+            val author: User,
+            val addedAt: String)
+
+    fun Article.render() = RenderedArticle(
+            slug,
+            title,
+            headline,
+            content,
+            author,
+            addedAt.format()
+    )
 }
