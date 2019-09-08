986f38c8df756f050d7d602abaf3bfbf1225b4ba
shinyay <shinya.com@gmail.com>
2019-09-08 21:52:22 +0900

    Modify fail to define repository type as Article

--- src/main/kotlin/io/pivotal/shinyay/repository/ArticleRepository.kt
+++ src/main/kotlin/io/pivotal/shinyay/repository/ArticleRepository.kt
@@ -1,12 +1,9 @@
 package io.pivotal.shinyay.repository
 
 import io.pivotal.shinyay.entity.Article
-import io.pivotal.shinyay.entity.User
 import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
 
-@Repository
-interface ArticleRepository : CrudRepository<User, Long> {
+interface ArticleRepository : CrudRepository<Article, Long> {
     fun findBySlug(slug: String): Article?
     fun findAllByOrderByAddedAtDesc(): Iterable<Article>
 }
