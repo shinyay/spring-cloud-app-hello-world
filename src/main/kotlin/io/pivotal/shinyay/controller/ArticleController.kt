c9cba7228d93aca1283b778b31abb5d426ac9153
shinyay <shinya.com@gmail.com>
2019-09-09 11:37:59 +0900

    Add rest api for finding article by slug

--- src/main/kotlin/io/pivotal/shinyay/controller/ArticleController.kt
+++ src/main/kotlin/io/pivotal/shinyay/controller/ArticleController.kt
@@ -2,6 +2,7 @@ package io.pivotal.shinyay.controller
 
 import io.pivotal.shinyay.repository.ArticleRepository
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -11,4 +12,8 @@ class ArticleController(val repository: ArticleRepository) {
 
     @GetMapping
     fun findAll() = repository.findAllByOrderByAddedAtDesc()
+
+    @GetMapping("/{slug}")
+    fun findOne(@PathVariable slug: String) =
+            repository.findBySlug(slug) ?: throw IllegalArgumentException("Wrong Slug")
 }
